617fa20a590a52ec325b1401364caa2b85c44e4c
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2020-02-25 12:52:34 -0600

    added values to enum

--- src/Main/Metadata/Qualities/MatchedLocationType.cs
+++ src/Main/Metadata/Qualities/MatchedLocationType.cs
@@ -2,19 +2,20 @@ namespace USC.GISResearchLab.Geocoding.Core.Metadata.Qualities
 {
     public enum MatchedLocationTypes
     {
-        Unknown,
-        StreetAddress,
-        PostOfficeBox,
-        RuralRoute,
-        StarRoute,
-        HighwayContractRoute,
-        Intersection,
-        NamedPlace,
-        RelativeDirection,
-        USPSZIP,
-        City,
-        State,
-        Unmatchable
+        Unmatchable = 0,
+        Unknown = 2,
+        StreetAddress = 4,
+        PostOfficeBox = 8,
+        RuralRoute = 16,
+        StarRoute = 32,
+        HighwayContractRoute = 64,
+        Intersection = 128,
+        NamedPlace = 256,
+        RelativeDirection = 512,
+        USPSZIP = 1024,
+        City = 2048,
+        State = 4096
+
     }
 
 
